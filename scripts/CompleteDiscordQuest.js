c1c6df8d46357560d4a51482ac9f412f4846cb35
xhos <60789741+xhos@users.noreply.github.com>
2025-10-06 00:39:57 +0000

    chore: update quest completion script

--- scripts/CompleteDiscordQuest.js
+++ scripts/CompleteDiscordQuest.js
@@ -7,7 +7,7 @@
  * This script automates the completion of Discord quests by spoofing game activity,
  * video watching, or streaming depending on the quest requirements.
  *
- * Last synced: 2025-10-02 21:25:12 UTC
+ * Last synced: 2025-10-06 00:39:56 UTC
  */
 
 delete window.$;
